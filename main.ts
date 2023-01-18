3079026140e2355d76302cf946d27f3e376672ea
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2023-01-18 15:39:06 +0100

    add nodemon with watch code functionality

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-import { httpMessage } from './src/functions/hello';
+import { httpMessage, inc, message } from './src/functions/hello';
 
 if (require.main === module) {
+  message();
+
+  let nb = 5;
+  console.log(nb);
+  nb = inc(nb);
+  console.log(nb);
+
   console.log(httpMessage('hello !'));
 }
