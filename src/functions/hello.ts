01eb156cffb666936e750073da5c723e9e1a1858
Laurent VANDELLE <vandellelaurent@yahoo.fr>
2023-12-04 00:01:10 +0100

    make eslint work

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,10 +1,10 @@
 import { HttpMessage } from '@models/hello.model';
 
-export function message() {
+export function message(): void {
   console.log('hello world !');
 }
 
-export function inc(x: number) {
+export function inc(x: number): number {
   return x + 1;
 }
 
